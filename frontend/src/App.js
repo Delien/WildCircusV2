0c81fdb7ae0ae9718c777a4b3e455d3dc171291b
Nathan Marcy <nathan.marcy@live.fr>
2019-07-17 17:37:15 +0200

    Commit somethings else.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import "./App.scss";
 import "./components/style/NavBar.scss";
+import "./components/style/Footer.scss";
 import { Container } from "reactstrap";
 import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
         <NavBar />
       </header>
       <Container />
+      <Footer />
     </React.Fragment>
   );
 }
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink
+} from "reactstrap";
+import StickyFooter from "react-sticky-footer";
+
+export default class Footer extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div className="footer">Toto</div>;
+  }
+}
